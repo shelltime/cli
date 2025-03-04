3a02c33c41a9801be785341f91d1287bacd9f4cd
AnnatarHe <annatar.he+github@gmail.com>
2025-03-04 23:49:42 +0800

    feat(alias): add alias import cli

---
+++ model/alias.go
@@ -0,0 +1,97 @@
+package model
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+	"os/user"
+
+	"github.com/sirupsen/logrus"
+)
+
+// Alias represents a shell alias
+type Alias struct {
+	Name  string `json:"name"`
+	Value string `json:"value"`
+	Shell string `json:"shell"`
+}
+
+type importShellAliasRequest struct {
+	Aliases       []string `json:"aliases" binding:"required" msg:"aliases"`
+	IsFullRefresh bool     `json:"isFullRefresh" msg:"isFullRefresh"`
+	ShellType     string   `json:"shellType" binding:"required" msg:"shellType"`
+	FileLocation  string   `json:"fileLocation" msg:"fileLocation"`
+
+	Hostname  string `json:"hostname" msg:"hostname"`
+	Username  string `json:"username" msg:"username"`
+	OS        string `json:"os" msg:"os"`
+	OSVersion string `json:"osVersion" msg:"osVersion"`
+}
+
+type importShellAliasResponse struct {
+	Success bool `json:"success"`
+	Count   int  `json:"count"`
+}
+
+// SendAliasesToServer sends the collected aliases to the server
+func SendAliasesToServer(ctx context.Context, endpoint Endpoint, aliases []string, isFullyRefresh bool, shellType, fileLocation string) error {
+	if len(aliases) == 0 {
+		logrus.Infoln("No aliases to send")
+		return nil
+	}
+
+	sysInfo, err := GetOSAndVersion()
+	if err != nil {
+		logrus.Warnln(err)
+		sysInfo = &SysInfo{
+			Os:      "unknown",
+			Version: "unknown",
+		}
+	}
+
+	hostname, err := os.Hostname()
+	if err != nil {
+		logrus.Warnln("Failed to get hostname:", err)
+		hostname = "unknown"
+	}
+
+	username := os.Getenv("USER")
+	if username == "" {
+		currentUser, err := user.Current()
+		if err != nil {
+			logrus.Warnln("Failed to get username:", err)
+			username = "unknown"
+		} else {
+			username = currentUser.Username
+		}
+	}
+
+	payload := importShellAliasRequest{
+		Aliases:       aliases,
+		IsFullRefresh: isFullyRefresh,
+		ShellType:     shellType,
+		FileLocation:  fileLocation,
+		Hostname:      hostname,
+		Username:      username,
+		OS:            sysInfo.Os,
+		OSVersion:     sysInfo.Version,
+	}
+
+	var resp importShellAliasResponse
+
+	err = SendHTTPRequest(HTTPRequestOptions[importShellAliasRequest, importShellAliasResponse]{
+		Context:  ctx,
+		Endpoint: endpoint,
+		Method:   http.MethodPost,
+		Path:     "/api/v1/import-alias",
+		Payload:  payload,
+		Response: &resp,
+	})
+	if err != nil {
+		return fmt.Errorf("failed to send aliases to server: %w", err)
+	}
+
+	logrus.Infoln("save aliases successfully", resp.Count)
+	return nil
+}
