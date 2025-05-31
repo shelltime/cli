8a33f80fef40a0e3891601499e54019db69f5d86
AnnatarHe <annatar.he+github@gmail.com>
2025-05-31 22:22:45 +0800

    feat(bash): add hook file existence check before bash shell installation

--- model/shell.bash.go
+++ model/shell.bash.go
@@ -37,6 +37,13 @@ func (s *BashHookService) ShellName() string {
 }
 
 func (s *BashHookService) Install() error {
+	hookFilePath := os.ExpandEnv(fmt.Sprintf("$HOME/%s/hooks/bash.bash", COMMAND_BASE_STORAGE_FOLDER))
+	if _, err := os.Stat(hookFilePath); os.IsNotExist(err) {
+		color.Red.Println("hook file not found at", hookFilePath)
+		color.Red.Println("Please run 'curl -sSL https://raw.githubusercontent.com/malamtime/installation/master/install.bash | bash' first")
+		return err
+	}
+
 	if _, err := os.Stat(s.configPath); os.IsNotExist(err) {
 		// Attempt to create .bashrc if it doesn't exist, as it's common for it not to be present by default on some systems
 		file, createErr := os.Create(s.configPath)
